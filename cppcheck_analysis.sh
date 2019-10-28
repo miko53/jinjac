157f76be244626b84954d43844347e98302c5dcd
Mickael Sergent <mickael-1.sergent@safrangroup.com>
2019-10-28 22:40:56 +0100

    path to include precised

--- cppcheck_analysis.sh
+++ cppcheck_analysis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cppcheck --enable=all -I include -j8 --language=c --platform=unix64 --std=c99 libjinjac/src
+cppcheck --enable=all -I libjinjac/include -j8 --language=c --platform=unix64 --std=c99 libjinjac/src
