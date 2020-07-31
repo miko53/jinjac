28002796055935c9d8f91495fc62116e2102bf92
Mickael Sergent <mickael.sergent@free.fr>
2020-07-31 17:15:35 +0200

    minor optimization remove duplication code

--- include/vm_opcode.h
+++ include/vm_opcode.h
@@ -23,48 +23,10 @@ extern const char* vm_buildin_fct[];
 
 typedef enum
 {
-  OP_COPY_STRING,      /// @offset static data, copy string to output stream
-  OP_NOP,
-  OP_HALT,             /// terminate vm
-  OP_PUSH_STRING,      /// @offset of data push it on stack
-  OP_PUSH_NIL,         /// insert @NULL pointer or @integer 0 into stack
-  OP_EVAL,             /// take j_value under top stack, convert to string and copy string to output stream
-  OP_CALL_BUILDIN_FCT, /// call buildin function @offset get the name of the function @nb_arg give the nb of argument
-  OP_LOAD_SYMBOL,      /// @offset name of identifier get object in stack
-  OP_LOAD_INTEGER,     /// @offset integer value pushed into stack
-  OP_LOAD_DOUBLE,      /// @offset double vale pushed into stack
-  OP_ADD,              /// do addition of value in stack (two) remove them and push result in stack
-  OP_SUB,              /// do subtraction of value in stack (two) remove them and push result in stack
-  OP_MUL,              /// do multiplication of value in stack (two) remove them and push result in stack
-  OP_DIV,              /// do division of value in stack (two) remove them and push result in stack
-  OP_GET_AT,           /// push onto stack the object given by object on stack at offset (given at @stack+1) @object --> reference @offset (convert in value)
-  OP_CREATE_SYMBOL,    /// @offset of identifier name
-  OP_DELETE_SYMBOL,    /// @offset of identifier name
-  OP_SET_SYMBOL,       /// @offset of identifier name, value is in top of @stack
-  OP_CREATE_ITERATOR,  /// create a iterator according to items in first level of @stack
-  OP_ITERATOR_LOAD_FIRST, /// from iterator obj from @stack, push the first items into @stack
-  OP_POP,
-  OP_ITERATOR_TEST_END_REACHED, /// check if end of iterator is reach iterator is in top of @stack, it push a boolean result in @tack
-  OP_B_TRUE,
-  OP_B_FALSE,
-  OP_JUMP,
-  OP_ITERATOR_INCR,
-  OP_ITERATOR_LOAD_NEXT,
-  OP_TEST_EQ,
-  OP_TEST_NEQ,
-  OP_TEST_GE,
-  OP_TEST_GT,
-  OP_TEST_LE,
-  OP_TEST_LT,
-  OP_TEST_OR,
-  OP_TEST_AND,
-  OP_TEST_NOT,
-  OP_TEST_IS,
-  OP_CREATE_TUPLE,
-  OP_CREATE_ARRAY,
+#define OPCODE(x) OP_ ## x,
+#include "ops.h"
+#undef OPCODE
 } VM_BYTE_CODE;
-
-extern const char* vm_byte_code[];
 
 #ifdef __cplusplus
 }
--- jinjacc/code.cpp
+++ jinjacc/code.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cstdarg>
 #include <cassert>
- 
+
 Code::Code()
 {
 
@@ -13,47 +13,11 @@ Code::~Code()
 
 }
 
-const char* vm_byte_code[] = 
+static const char* vm_byte_code[] =
 {
-  "OP_COPY_STRING",
-  "OP_NOP",
-  "OP_HALT",
-  "OP_PUSH_STRING",
-  "OP_PUSH_NIL",
-  "OP_EVAL",
-  "OP_CALL_BUILDIN_FCT",
-  "OP_LOAD_SYMBOL",
-  "OP_LOAD_INTEGER",
-  "OP_LOAD_DOUBLE",
-  "OP_ADD",
-  "OP_SUB",
-  "OP_MUL",
-  "OP_DIV",
-  "OP_GET_AT",
-  "OP_CREATE_SYMBOL",
-  "OP_DELETE_SYMBOL",
-  "OP_SET_SYMBOL",
-  "OP_CREATE_ITERATOR",
-  "OP_ITERATOR_LOAD_FIRST",
-  "OP_POP",
-  "OP_ITERATOR_TEST_END_REACHED",
-  "OP_B_TRUE",
-  "OP_B_FALSE",
-  "OP_JUMP",
-  "OP_ITERATOR_INCR",
-  "OP_ITERATOR_LOAD_NEXT",
-  "OP_TEST_EQ",
-  "OP_TEST_NEQ",
-  "OP_TEST_GE",
-  "OP_TEST_GT",
-  "OP_TEST_LE",
-  "OP_TEST_LT",
-  "OP_TEST_OR",
-  "OP_TEST_AND",
-  "OP_TEST_NOT",
-  "OP_TEST_IS",
-  "OP_CREATE_TUPLE",
-  "OP_CREATE_ARRAY",
+#define OPCODE(x) #x,
+#include "ops.h"
+#undef OPCODE
 };
 
 uint32_t Code::insertvStatement(VM_BYTE_CODE opcode, va_list args)
@@ -61,131 +25,17 @@ uint32_t Code::insertvStatement(VM_BYTE_CODE opcode, va_list args)
   int32_t arg1;
   double arg2;
   uint32_t offset;
-  
+
   std::cout << vm_byte_code[opcode] << "  ";
   offset = m_bytecode.insert((int8_t) opcode);
-  switch(opcode)
+  switch (opcode)
   {
     case OP_COPY_STRING:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-      
-    case OP_NOP: //no argument
-    case OP_HALT: 
-    case OP_EVAL:
-    case OP_PUSH_NIL:
-    case OP_ADD:
-    case OP_SUB:
-    case OP_MUL:
-    case OP_DIV:
-    case OP_GET_AT:
-    case OP_CREATE_ITERATOR:
-    case OP_ITERATOR_LOAD_FIRST:
-    case OP_POP:
-    case OP_ITERATOR_TEST_END_REACHED:
-    case OP_ITERATOR_INCR:
-    case OP_ITERATOR_LOAD_NEXT:
-    case OP_TEST_EQ:
-    case OP_TEST_NEQ:
-    case OP_TEST_GE:
-    case OP_TEST_GT:
-    case OP_TEST_LE:
-    case OP_TEST_LT:
-    case OP_TEST_OR:
-    case OP_TEST_AND:
-    case OP_TEST_NOT:
-    case OP_TEST_IS:
-    case OP_CREATE_TUPLE:
-    case OP_CREATE_ARRAY:
-      break;
-      
-    case OP_PUSH_STRING:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-                  
-    case OP_CALL_BUILDIN_FCT:
-    {
-      int32_t nbArgs;
-      arg1 = va_arg(args, int32_t);
-      nbArgs = va_arg(args, int32_t);
-      std::cout << arg1 << " " << nbArgs;
-      m_bytecode.insert(arg1);
-      m_bytecode.insert(nbArgs);
-    }
-      break;
-    
-    case OP_LOAD_SYMBOL:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-    
-    case OP_LOAD_INTEGER:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-      
-    case OP_LOAD_DOUBLE:
-      arg2 = va_arg(args, double);
-      std::cout << arg2;
-      m_bytecode.insert(arg2);
-      break;
-      
-    case OP_CREATE_SYMBOL:
-    case OP_DELETE_SYMBOL:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-    
-    case OP_SET_SYMBOL:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
 
-    case OP_B_TRUE:
-    case OP_JUMP:
-    case OP_B_FALSE:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-      
-    default:
-      assert(false);
-      break;
-  }
-  
-  std::cout << std::endl;
-  
-  va_end(args);
-  return offset;  
-}
-
-uint32_t Code::insertStatement(VM_BYTE_CODE opcode, ...)
-{
-  va_list args;
-  va_start(args, opcode);  
-  int32_t arg1;
-  double arg2;
-  uint32_t offset;
-  
-  std::cout << vm_byte_code[opcode] << "  ";
-  offset = m_bytecode.insert((int8_t) opcode);
-  switch(opcode)
-  {
-    case OP_COPY_STRING:
-      arg1 = va_arg(args, int32_t);
-      std::cout << arg1;
-      m_bytecode.insert(arg1);
-      break;
-      
     case OP_NOP: //no argument
     case OP_HALT:
     case OP_EVAL:
@@ -214,49 +64,49 @@ uint32_t Code::insertStatement(VM_BYTE_CODE opcode, ...)
     case OP_CREATE_TUPLE:
     case OP_CREATE_ARRAY:
       break;
-      
+
     case OP_PUSH_STRING:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-                  
+
     case OP_CALL_BUILDIN_FCT:
-    {
-      int32_t nbArgs;
-      arg1 = va_arg(args, int32_t);
-      nbArgs = va_arg(args, int32_t);
-      std::cout << arg1 << " " << nbArgs;
-      m_bytecode.insert(arg1);
-      m_bytecode.insert(nbArgs);
-    }
+      {
+        int32_t nbArgs;
+        arg1 = va_arg(args, int32_t);
+        nbArgs = va_arg(args, int32_t);
+        std::cout << arg1 << " " << nbArgs;
+        m_bytecode.insert(arg1);
+        m_bytecode.insert(nbArgs);
+      }
       break;
-    
+
     case OP_LOAD_SYMBOL:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-    
+
     case OP_LOAD_INTEGER:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-      
+
     case OP_LOAD_DOUBLE:
       arg2 = va_arg(args, double);
       std::cout << arg2;
       m_bytecode.insert(arg2);
       break;
-      
+
     case OP_CREATE_SYMBOL:
     case OP_DELETE_SYMBOL:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-    
+
     case OP_SET_SYMBOL:
       arg1 = va_arg(args, int32_t);
       std::cout << arg1;
@@ -270,23 +120,31 @@ uint32_t Code::insertStatement(VM_BYTE_CODE opcode, ...)
       std::cout << arg1;
       m_bytecode.insert(arg1);
       break;
-      
+
     default:
       assert(false);
       break;
   }
-  
+
   std::cout << std::endl;
-  
+
   va_end(args);
   return offset;
+}
+
+uint32_t Code::insertStatement(VM_BYTE_CODE opcode, ...)
+{
+  va_list args;
+  va_start(args, opcode);
+
+  return insertvStatement(opcode, args);
 }
 
 uint32_t Code::replaceStatement(uint32_t offset, VM_BYTE_CODE opcode, ...)
 {
   va_list args;
-  va_start(args, opcode);  
-  
+  va_start(args, opcode);
+
   int32_t currentOffset = m_bytecode.getCurrent();
   m_bytecode.setCurrent(offset);
   insertvStatement(opcode, args);
@@ -294,7 +152,6 @@ uint32_t Code::replaceStatement(uint32_t offset, VM_BYTE_CODE opcode, ...)
   va_end(args);
   return 0;
 }
-
 
 void Code::write(std::ofstream* outFile)
 {
